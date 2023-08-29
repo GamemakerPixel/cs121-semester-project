2ab69dd4e02b6690d14f65e935e1398d44358907
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2023-08-29 13:42:12 -0400

    Completed DialogDemonstration
    - Demonstrated Dialog Boxes

---
+++ src/weekTwo/DialogDemonstration.java
@@ -0,0 +1,11 @@
+package weekTwo;
+
+import javax.swing.*;
+
+public class DialogDemonstration {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter a name: ");
+        JOptionPane.showMessageDialog(null,"Your name is " + name);
+
+    }
+}
