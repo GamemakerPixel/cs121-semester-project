4311a9d6261614837dbab421ef724cc4480e6f92
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2023-09-19 13:42:30 -0400

    Finished FileRead.java

--- src/week5/fileActivity/FileRead.java
+++ src/week5/fileActivity/FileRead.java
@@ -3,12 +3,22 @@ package week5.fileActivity;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class FileRead {
+
+    private static final String filePath = "courses.txt";
+    private static final Pattern pattern = Pattern.compile(".*\t*");
+
     public static void main(String[] args) {
         try{
-            File coursesFile = new File("courses.txt");
+            File coursesFile = new File(filePath);
             Scanner coursesScanner = new Scanner(coursesFile);
+
+            while (coursesScanner.hasNext(pattern)){
+
+                System.out.println(coursesScanner.next(pattern));
+            }
         }
         catch (FileNotFoundException exception){
             exception.printStackTrace();
