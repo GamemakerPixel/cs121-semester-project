4321efa8457643f9dfde219cbf41235a85162fbe
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2023-09-22 19:59:51 -0400

    promptForData now almost works

--- src/week5/fileActivity/FileWrite.java
+++ src/week5/fileActivity/FileWrite.java
@@ -10,7 +10,9 @@ public class FileWrite{
   public static void main(String[] args) {
     ArrayList<String> headers = promptForHeaders();
 
-    promptForData(headers);
+    ArrayList<ArrayList<Object>> data = promptForData(headers);
+
+    System.out.println(data);
   }
 
   private static ArrayList<String> promptForHeaders(){
@@ -32,19 +34,34 @@ public class FileWrite{
   }
 
   private static ArrayList<ArrayList<Object>> promptForData(ArrayList<String> headers){
-    ArrayList<ArrayList<Object>> data = ArrayList<>();
+    ArrayList<ArrayList<Object>> data = new ArrayList<>();
 
     for (String header : headers){
-      data.append(promptDatatype(header));
+      data.add(promptDatatype(header));
     }
+
+    return data;
   }
 
   private static ArrayList<Object> promptDatatype(String header){
     
     while (true){
-      System.out.printf("Enter the datatype for the %s column. (i:int, d:double, s:String)");
+      System.out.printf("Enter the datatype for the %s column. (i:int, d:double, s:String)", header);
 
-      String input = TYPE HERE :)
+      String input = scanner.nextLine();
+
+      switch(input){
+        case "i":
+          return new ArrayList<Integer>();
+          break;
+        case "d":
+          return new ArrayList<Double>();
+          break;
+        case "s":
+          return new ArrayList<String>();
+        default:
+          continue;
+      }
     }
 
     
