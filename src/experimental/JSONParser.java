4f188a0a43e881d6e093990c36a03ec91f2696ff
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2023-10-06 00:38:11 -0400

    Started JSONParser

---
+++ src/experimental/JSONParser.java
@@ -0,0 +1,10 @@
+package experimental;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class JSONParser{
+  public static HashMap<String, Object> parseObject(String object){
+    
+  }
+}
