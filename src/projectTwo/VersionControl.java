d751059df2661cf7b0b628956336767dfe032309
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2023-11-30 00:27:37 -0500

    Started Project Two

---
+++ src/projectTwo/VersionControl.java
@@ -0,0 +1,16 @@
+package projectTwo;
+
+import java.util.HashMap;
+
+// Inspired by git
+
+public class VersionControl{
+  private enum Command{
+    INITIALIZE,
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
